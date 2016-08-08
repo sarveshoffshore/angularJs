1f975e825d0a038748465d71ffc8fef62cd18041
sarveshoffshore <sarvesh@offshorewebmaster.com>
2016-08-08 14:49:05 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+<body>
+
+<div ng-app="" ng-init="names=['Sarvesh Patel','Mrugesh Bhatt','Hitesh Patel']">
+  <p>Looping with ng-repeat:</p>
+  <ul>
+    <li ng-repeat="x in names">
+      {{ x }}
+    </li>
+  </ul>
+</div>
+
+</body>
+</html>
+
